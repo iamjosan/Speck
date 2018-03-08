83589e0c0fa327cfb0a78fa1e2675b4a681d49a6
Josan Iracheta <josan.iracheta@gmail.com>
2018-03-08 00:28:55 -0800

    app is ready for deployment

--- server/index.js
+++ server/index.js
@@ -1,13 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
-app.use('/scripts', express.static(__dirname + '/node_modules/chart.js/dist/'));
-app.set('view engine', 'ejs');
-app.set('views','./views');
+app.use("/scripts", express.static(__dirname + "/node_modules/chart.js/dist/"));
+app.set("view engine", "ejs");
+app.set("views", "./views/pages");
 
-app.get('/', (req, res) => {
-	res.render('index');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.post("/results", (req, res) => {
+  console.log("results page");
+  console.log(req.body);
+  const resultsAll = [];
+  for (let key in req.body) {
+    resultsAll.push(req.body[key]);
+  }
+  //render view with manipulated data
+  res.render("results", { results: JSON.stringify(resultsAll) });
+});
+
+app.get("/", (req, res) => {
+  res.render("form");
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('Listening on port: ', port));
+app.listen(port, () => console.log("Listening on port: ", port));
